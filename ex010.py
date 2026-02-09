8fbfc89a59e85d455500bf525c4a0918aedb40ca
Davi Machado <davimachado.ep@gmail.com>
2026-02-08 22:16:28 -0300

    atualização do código

--- ex010.py
+++ ex010.py
@@ -1,3 +1,3 @@
 #Programa que lê quantos reais uma pessoa tem no bolso; e passa o quanto ela pode trocar por dólar. Considerando US$ 1,00 e R$3,27
-real= float(input('Quantos reais você tem?'))
-print('Você tem R${} e pode trocar por US${} '.format(real, real*0.3))
+real = float(input('Quantos reais você tem?'))
+print(f'Você tem R${real:.2f} e pode trocar por US${real/3.27:.2f}')
