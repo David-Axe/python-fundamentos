b0a1d49f3d2075547cc3126a775489183a5b0102
Davi Machado <davimachado.ep@gmail.com>
2026-02-17 19:12:47 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,5 +1,5 @@
 #Programa que lê o nome de uma pessoa e mostre: o nome com todas as letras maiúsculas, minúsculas, quantas letras tem ao todo (sem considerar espaços), quantas letras tem o primeiro nome.
-nome = str(input('Digite o seu nome: '))
+nome = str(input('Digite o seu nome completo: '))
 print(nome.upper())
 print(nome.lower())
 print(len(nome.replace(' ','')))
