b8721e63097ad3c40b078e749e84ab88efc843c8
Davi Machado <davimachado.ep@gmail.com>
2026-03-14 10:13:45 -0300

    Update ex003.py

--- ex003.py
+++ ex003.py
@@ -1,4 +1,4 @@
 n1= int(input('Digite algum número: '))
 n2= int(input('Agora, outro número: '))
 soma = n1 + n2
-print('A soma entre {} e {} vale {}!'.format(n1,n2,soma))
+print('A soma entre {} e {} vale {}{}{}!'.format(n1,n2,'\033[1;31m', soma, '\033[m'))
